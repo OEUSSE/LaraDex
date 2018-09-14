9cfff0295856106c854dab8247ff2f0edf0409da
Oscar Eusse Florez <scr.eusse@gmail.com>
2018-09-14 16:53:24 -0500

    Ã[Add] Se agregaron estilos a la vista de los entrenadores

--- resources/views/trainers/index.blade.php
+++ resources/views/trainers/index.blade.php
@@ -6,8 +6,11 @@
     <div class="row">
     @foreach ($trainers as $trainer)
         <div class="col-sm-4">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="images/{{$trainer->avatar}}"} alt="Card image cap">
+            <div class="card text-center" style="width: 18rem; margin: 30px auto;">
+                <img style="height: 100px; width: 100px; background-color: lightgray; margin: 10px;"
+                    class="card-img-top rounded-circle mx-auto d-block" 
+                    src="images/{{$trainer->avatar}}"} 
+                    alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">{{$trainer->name}}</h5>
                     <p class="card-text">{{$trainer->description}}</p>
