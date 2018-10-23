1d5ac8a7d33cc882742e23915903f8c6fd88b4ed
Oscar Eusse Florez <scr.eusse@gmail.com>
2018-10-23 17:45:41 -0500

    [Mod] Obteniendo el middleware desde el kernel

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LaraDex\Http\Middleware;
+
+use Closure;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->age <= 200) {
+            return redirect('/trainers');
+        }
+
+        return $next($request);
+    }
+}
